3ef17b6f77da292364151b4e665190a5de0c1c66
matlaczj <56789773+matlaczj@users.noreply.github.com>
2021-12-11 16:55:41 +0100

    changes to plot_validation_curve

--- Projekt/our_library/our_reusable_methods.py
+++ Projekt/our_library/our_reusable_methods.py
@@ -2,7 +2,8 @@ from sklearn.model_selection import validation_curve
 import matplotlib.pyplot as plt
 import numpy as np
     
-def plot_validation_curve(model,model_name,X,y,param_range,param_name,scoring,cv=5,verbose=0):
+def plot_validation_curve(model,model_name,X,y,param_range,param_name,scoring,cv=5,
+                          verbose=0,xscale="log",lw = 1.5,opacity=0.5,figsize=(10,5)):
     """
     This function cross_validated training data and plots nice graphs for training 
     as well as for validation for each hiperparameter. It shows std of scores in each split.
@@ -18,7 +19,11 @@ def plot_validation_curve(model,model_name,X,y,param_range,param_name,scoring,cv
     param_name - very specific name of hiperparameter (same as field in model's class).
     scoring - very specific sklearn predefined scoring function name.
     model_name - just the name of model to show on plot.
+    xscale - depending on param_range it may be best to use "log" or "lin".
     cv - number of splits in cross-validation. 
+    lw - line width.
+    opacity - .
+    figsize - change size of the graph, accepts (width,height).
     """
 
     train_scores, validation_scores = validation_curve(
@@ -35,32 +40,39 @@ def plot_validation_curve(model,model_name,X,y,param_range,param_name,scoring,cv
     validation_scores_mean = np.mean(validation_scores, axis=1)
     validation_scores_std = np.std(validation_scores, axis=1)
 
-    plt.title(f"Validation Curve With {model_name}")
-    plt.xlabel(param_name)
-    plt.ylabel("Score")
+    fig,ax = plt.subplots(1)
+    fig.set_figwidth(figsize[0])
+    fig.set_figheight(figsize[1])
 
-    lw = 1.5
-    plt.semilogx(
+    ax.set_title(f"Validation Curve With {model_name}")
+    ax.set_xlabel(param_name)
+    ax.set_ylabel("Score")
+    
+    ax.plot(
         param_range, train_scores_mean, 
         label="Training score", color="darkorange", lw=lw
     )
-    plt.fill_between(
+    ax.fill_between(
         param_range, 
         train_scores_mean - train_scores_std,
         train_scores_mean + train_scores_std,
-        alpha=0.1, color="darkorange", lw=lw
+        alpha=opacity, color="darkorange", lw=lw
     )
-    plt.semilogx(
+    ax.plot(
         param_range, validation_scores_mean, 
-        label="Cross-validation score", color="navy", lw=lw
+        label="Cross-validation score", color="navy", lw=lw,
     )
-    plt.fill_between(
+    ax.fill_between(
         param_range,
         validation_scores_mean - validation_scores_std,
         validation_scores_mean + validation_scores_std,
-        alpha=0.1, color="navy", lw=lw
+        alpha=opacity, color="navy", lw=lw
     )
-    plt.legend(loc="best")
+    
+    if(xscale == "log"):
+        ax.set_xscale('log')
+
+    ax.legend(loc="best")
     plt.show()
 
     return train_scores_mean, validation_scores_mean
