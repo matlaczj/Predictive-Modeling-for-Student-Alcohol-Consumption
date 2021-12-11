6d4f53fc10880f52107e3a77986633b57ba5829d
matlaczj <56789773+matlaczj@users.noreply.github.com>
2021-12-11 16:37:38 +0100

    użycie plot_validation_curve
    redukcja kodu i poprawa jakości badań dzięki nowej funkcji plot_validation_curve

--- Projekt/our_library/our_reusable_methods.py
+++ Projekt/our_library/our_reusable_methods.py
@@ -2,14 +2,19 @@ from sklearn.model_selection import validation_curve
 import matplotlib.pyplot as plt
 import numpy as np
     
-def plot_validation_curve(X,y,param_range,model,param_name,scoring,model_name,cv=5):
+def plot_validation_curve(model,model_name,X,y,param_range,param_name,scoring,cv=5,verbose=0):
     """
     This function cross_validated training data and plots nice graphs for training 
     as well as for validation for each hiperparameter. It shows std of scores in each split.
+    
+    This function will make sense as long as high score is a good thing. Thats why 
+    sklearn has built in score neg_mean_absolute_error to invert the logic and turn error
+    that is best low into score that is best high.
 
+    Arguments:
     X,y - data to be fitted on model (training data).
     model - model to use (without specified hiperparameters).
-    param_range - range of parameters to use (in logarithmic space).
+    param_range - range of parameters to use (in logarithmic space). Example: np.logspace(-2, 4, 50)
     param_name - very specific name of hiperparameter (same as field in model's class).
     scoring - very specific sklearn predefined scoring function name.
     model_name - just the name of model to show on plot.
@@ -21,7 +26,8 @@ def plot_validation_curve(X,y,param_range,model,param_name,scoring,model_name,cv
         param_name=param_name,
         param_range=param_range,
         scoring=scoring,
-        cv=cv
+        cv=cv,
+        verbose=verbose
     )
 
     train_scores_mean = np.mean(train_scores, axis=1)
@@ -56,3 +62,5 @@ def plot_validation_curve(X,y,param_range,model,param_name,scoring,model_name,cv
     )
     plt.legend(loc="best")
     plt.show()
+
+    return train_scores_mean, validation_scores_mean
